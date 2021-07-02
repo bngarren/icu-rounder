57f690934f6966d7edd4a799d227f13b40a87264
Ben Garren <bngarren@gmail.com>
2021-07-02 01:34:42 +0000

    Closes #28.  Added type checking and coercion to Importer and Exporter components. Primarily for number -> string conversion, but could incorporate other safeties in the future

--- src/components/Exporter/index.js
+++ src/components/Exporter/index.js
@@ -1,13 +1,13 @@
-import { useEffect, useState, cloneElement } from "react";
+import { cloneElement } from "react";
 import { saveAs } from "file-saver";
 
 import { useGridStateContext } from "../../context/GridState";
 
 const exportDataWithFilename = (data, filename) => {
-  const blob = new Blob([JSON.stringify(data, null, 2)], {
-    type: "application/json",
-  });
-  saveAs(blob, `${filename}.json`);
+   const blob = new Blob([JSON.stringify(data, null, 2)], {
+      type: "application/json",
+   });
+   saveAs(blob, `${filename}.json`);
 };
 
 /* This component will add an onClick function to its children component that
@@ -15,20 +15,38 @@ will export the grid data to json.
 E.g. If Exporter surrounds a Button component it will add onClick function
 */
 const Exporter = ({ children, filename = "grid", onExported = (f) => f }) => {
-  const [data, setData] = useState(); // local data in the Exporter component
-  const { gridDataJson } = useGridStateContext();
+   const { gridDataJson } = useGridStateContext();
 
-  useEffect(() => {
-    // Get gridData from the GridStateContext
-    setData(gridDataJson);
-  }, [gridDataJson]);
+   const handleExport = () => {
+      try {
+         exportDataWithFilename(getModifiedData(gridDataJson), filename);
+      } catch (err) {
+         console.error(`Error in Exporter: ${err}`);
+      } finally {
+         onExported();
+      }
+   };
 
-  const handleExport = () => {
-    exportDataWithFilename(data, filename);
-    onExported();
-  };
+   return cloneElement(children, { onClick: handleExport });
+};
 
-  return cloneElement(children, { onClick: handleExport });
+/* Helper function to get cleaned/type coerced data for export */
+const getModifiedData = (rawData) => {
+   let modifiedData = rawData;
+
+   // for each bedspace of the gridData object
+   Object.values(modifiedData).forEach((element) => {
+      let modifiedBedElement = element;
+      // for each property of the bed object
+      Object.keys(element).forEach((k) => {
+         // convert any numbers to strings
+         if (typeof element[k] === "number") {
+            modifiedBedElement[k] = element[k] + "";
+         }
+      });
+      modifiedData[element] = modifiedBedElement;
+   });
+   return modifiedData;
 };
 
 export default Exporter;
